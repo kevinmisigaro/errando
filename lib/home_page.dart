e31232b4b06ba6d19b6ed8d02dd8a811b936ccf8
Kevin Misigaro <kmisigaro@outlook.com>
2019-10-28 22:48:59 +0300

    First commit

---
+++ lib/home_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+      child: Container(
+        margin: const EdgeInsets.only(right: 45, left: 45),
+        child: Column(
+          children: <Widget>[
+
+
+            Padding(padding: EdgeInsets.symmetric(vertical: 10.0), ),
+
+
+            Image.asset(
+              'assets/images/errando-proper.png',
+              scale: 1.9,
+              ),
+
+
+            Padding(padding: EdgeInsets.symmetric(vertical: 20.0), ),
+
+
+            Text(
+              'SIGN IN TO ERRANDO', 
+              style: TextStyle(
+                color: Colors.white, 
+                fontSize:  25.0,
+                fontWeight: FontWeight.bold
+                ), 
+            ),
+
+
+            Padding(padding: EdgeInsets.symmetric(vertical: 20.0), ),
+
+
+            TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: "Username",
+              hintStyle: TextStyle(fontSize: 19),
+              contentPadding: EdgeInsets.all(17.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+            ),
+          ),
+
+
+          Padding(padding: EdgeInsets.symmetric(vertical: 15.0), ),
+
+           TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: "Password",
+              hintStyle: TextStyle(fontSize: 19),
+              contentPadding: EdgeInsets.all(17.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+            ),
+          ),
+
+          Padding(padding: EdgeInsets.symmetric(vertical: 30.0), ),
+
+          FlatButton(
+            color: Colors.orange,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.fromLTRB(125, 15, 125, 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50)
+            ),
+            splashColor: Colors.deepOrange,
+            onPressed: () {
+              /*...*/
+            },
+            child: Text(
+              "LOGIN",
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+
+          Padding(padding: EdgeInsets.symmetric(vertical: 30.0), ),
+
+          Text('Forgot Password?', style: TextStyle(
+            color: Colors.white54,
+            fontSize: 19.0
+            ),
+            ),
+
+            Padding(padding: EdgeInsets.symmetric(vertical: 15.0), ),
+
+            Text('Not Registered? Sign Up!', style: TextStyle(
+            color: Colors.white54,
+            fontSize: 19.0
+            ),
+            ),
+
+          ],
+        ),
+      ),
+    )
+      );
+  }
+}
